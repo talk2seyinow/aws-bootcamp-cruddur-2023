8cf4338d0ad43d9ac5a007dddc02dc20a053a7de
Seyi Raimi <talk2seyinow@gmail.com>
2023-03-16 08:27:57 +0000

    authorization header

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.user_activities import *
@@ -80,8 +81,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -150,7 +151,12 @@ def data_create_message():
   return
 
 @app.route("/api/activities/home", methods=['GET'])
+@xray_recorder.capture('activities_home')
 def data_home():
+  app.logger.debug("AUTH HEADER")
+  app.logger.debug(
+    request.headers.get('Authorization')
+  )
   data = HomeActivities.run()
   return data, 200
 
